c31491c4ae32a3c476152cccab963b45554d4175
BOOMS17 <judahroberts100@gmail.com>
2024-07-01 18:46:16 -0400

    .

--- script2.js
+++ script2.js
@@ -5,7 +5,7 @@ let ChooseAdv = document.querySelector('.cyoa')
 
 MultChoice.addEventListener('click', function() {
 
-    window.open('https://github.com/BOOMS17/quiz', "_blank")
+    window.open('http://127.0.0.1:5500/', "_blank")
    
 
 })
