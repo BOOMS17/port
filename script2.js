b3e725f74eb7a426fbd3d8ef6bcbebc03ee46e34
BOOMS17 <judahroberts100@gmail.com>
2024-07-01 18:40:42 -0400

    .

--- script2.js
+++ script2.js
@@ -12,6 +12,6 @@ MultChoice.addEventListener('click', function() {
 
 ChooseAdv.addEventListener('click', function() {
 
-    window.open('https://github.com/BOOMS17/adventure', "_blank")
+    window.open('http://127.0.0.1:5500/', "_blank")
     
 })
